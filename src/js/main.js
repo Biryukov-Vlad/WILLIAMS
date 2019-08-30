c92f2cde1ca62daa819a6f55ec4cf58b17265c31
Vlad <ksand.1@ya.ru>
2019-08-30 14:59:03 +0400

    заливаю первую готовую страничку перед сборкой остальных

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,22 @@
-let anchors = document.querySelectorAll('a[href*="#"]');
+scrollAnchor();      // скрол у якоря
+rating();            // рейтинг компании
+slideReviews();   // слайдер - swiper   
+// sliderPosts()
+function scrollAnchor() {
+    let anchors = document.querySelectorAll('a[href*="#"]');
 
-for (let anchor of anchors) {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault()
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault()
 
-        const blockID = anchor.getAttribute('href')
+            const blockID = anchor.getAttribute('href')
 
-        document.querySelector('' + blockID).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
         })
-    })
+    }
 }
 
 var hamburger = document.getElementsByClassName("header__hamburger")[0],
@@ -21,117 +27,147 @@ hamburger.onclick = function () {
     document.body.classList.toggle("open-menu")
 }
 
+function rating() {
+    var star,
+        quantity = 0,
+        checkClickStars = false,
+        star_1 = document.getElementsByClassName("footer__star")[0],
+        star_2 = document.getElementsByClassName("footer__star")[1],
+        star_3 = document.getElementsByClassName("footer__star")[2],
+        star_4 = document.getElementsByClassName("footer__star")[3],
+        star_5 = document.getElementsByClassName("footer__star")[4];
 
-var star,
-    quantity = 0,
-    checkClickStars = false,
-    star_1 = document.getElementsByClassName("footer__star")[0],
-    star_2 = document.getElementsByClassName("footer__star")[1],
-    star_3 = document.getElementsByClassName("footer__star")[2],
-    star_4 = document.getElementsByClassName("footer__star")[3],
-    star_5 = document.getElementsByClassName("footer__star")[4];
 
 
+    star_1.onclick = function () {
+        if (!checkClickStars) {
+            quantity = 1;
+            checkClickStars = true;
+            ratingCompany(quantity);
+        }
+    }
+    star_2.onclick = function () {
+        if (!checkClickStars) {
+            quantity = 2;
+            checkClickStars = true;
+            ratingCompany(quantity);
+        }
+    }
+    star_3.onclick = function () {
+        if (!checkClickStars) {
+            quantity = 3;
+            checkClickStars = true;
+            ratingCompany(quantity);
+        }
+    }
+    star_4.onclick = function () {
+        if (!checkClickStars) {
+            quantity = 4;
+            checkClickStars = true;
+            ratingCompany(quantity);
+        }
+    }
+    star_5.onclick = function () {
+        if (!checkClickStars) {
+            quantity = 5;
+            checkClickStars = true;
+            ratingCompany(quantity);
+        }
+    }
 
-star_1.onclick = function () {
-    if (!checkClickStars) {
-        quantity = 1;
-        checkClickStars = true;
-        ratingCompany(quantity);
+    function ratingCompany(quantity) {
+        for (var i = 0; i < quantity; i++) {
+            star = document.getElementsByClassName("footer__star")[i];
+            if (!star.classList.contains("footer__star-activ-js")) {
+                star.classList.add("footer__star-activ-js");
+            }
+        }
     }
-}
-star_2.onclick = function () {
-    if (!checkClickStars) {
-        quantity = 2;
-        checkClickStars = true;
-        ratingCompany(quantity);
-    }
-}
-star_3.onclick = function () {
-    if (!checkClickStars) {
-        quantity = 3;
-        checkClickStars = true;
-        ratingCompany(quantity);
-    }
-}
-star_4.onclick = function () {
-    if (!checkClickStars) {
-        quantity = 4;
-        checkClickStars = true;
-        ratingCompany(quantity);
-    }
-}
-star_5.onclick = function () {
-    if (!checkClickStars) {
-        quantity = 5;
-        checkClickStars = true;
-        ratingCompany(quantity);
-    }
-}
 
-function ratingCompany(quantity) {
-    for (var i = 0; i < quantity; i++) {
-        star = document.getElementsByClassName("footer__star")[i];
-        if (!star.classList.contains("footer__star-activ-js")) {
-            star.classList.add("footer__star-activ-js");
+    star_1.onmouseover = function () {
+        if (!checkClickStars) {
+            quantity = 1;
+            removeStar()
+            targetStar(quantity)
+        }
+    }
+    star_2.onmouseover = function () {
+        if (!checkClickStars) {
+            quantity = 2;
+            removeStar()
+            targetStar(quantity)
+        }
+    }
+    star_3.onmouseover = function () {
+        if (!checkClickStars) {
+            quantity = 3;
+            removeStar()
+            targetStar(quantity)
+        }
+    }
+    star_4.onmouseover = function () {
+        if (!checkClickStars) {
+            quantity = 4;
+            removeStar()
+            targetStar(quantity)
+        }
+    }
+    star_5.onmouseover = function () {
+        if (!checkClickStars) {
+            quantity = 5;
+            removeStar()
+            targetStar(quantity)
+        }
+    }
+
+    function targetStar() {
+        for (var i = 0; i < quantity; i++) {
+            star = document.getElementsByClassName("footer__star")[i];
+            if (!star.classList.contains("footer__star-activ-js")) {
+                star.classList.add("footer__star-activ-js");
+            }
+        }
+    }
+
+    function removeStar() {
+        for (var i = 0; i < 5; i++) {
+            star = document.getElementsByClassName("footer__star")[i];
+            if (!!star.classList.contains("footer__star-activ-js")) {
+                star.classList.remove("footer__star-activ-js");
+            }
         }
     }
 }
 
-star_1.onmouseover = function () {
-    if (!checkClickStars) {
-        quantity = 1;
-        removeStar()
-        targetStar(quantity)
-    }
-}
-star_2.onmouseover = function () {
-    if (!checkClickStars) {
-        quantity = 2;
-        removeStar()
-        targetStar(quantity)
-    }
-}
-star_3.onmouseover = function () {
-    if (!checkClickStars) {
-        quantity = 3;
-        removeStar()
-        targetStar(quantity)
-    }
-}
-star_4.onmouseover = function () {
-    if (!checkClickStars) {
-        quantity = 4;
-        removeStar()
-        targetStar(quantity)
-    }
-}
-star_5.onmouseover = function () {
-    if (!checkClickStars) {
-        quantity = 5;
-        removeStar()
-        targetStar(quantity)
-    }
-}
 
-function targetStar() {
-    for (var i = 0; i < quantity; i++) {
-        star = document.getElementsByClassName("footer__star")[i];
-        if (!star.classList.contains("footer__star-activ-js")) {
-            star.classList.add("footer__star-activ-js");
-        }
-    }
-}
 
-function removeStar() {
-    for (var i = 0; i < 5; i++) {
-        star = document.getElementsByClassName("footer__star")[i];
-        if (!!star.classList.contains("footer__star-activ-js")) {
-            star.classList.remove("footer__star-activ-js");
-        }
-    }
+// СЛАЙДЕР 
+
+
+function slideReviews() {
+    var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 5500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 }
 
 
-
-
+function sliderPosts() {
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+}
